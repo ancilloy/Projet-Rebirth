15aa4e9fbdf6f91a21b949ea68823c17c927dce1
Barthélemy Brodzinski <166053562+SemolinaWtvr@users.noreply.github.com>
2024-09-22 15:44:15 +0200

    Create Rebirth.php

---
+++ Rebirth.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Title</title>
+</head>
+<body>
+<h1>Project Rebirth</h1>
+<table class="CharactersTable">
+    <tr>
+        <th>
+            <img src="image/Angie_img.png" alt="Angie">
+        </th>
+        <th>
+            <img src="image/Gonta_img.png" alt="Gonta">
+        </th>
+        <th>
+            <img src="image/Himiko_img.png" alt="Himiko">
+        </th>
+        <th>
+            <img src="image/Kaede_img.png" alt="Kaede">
+        </th>
+    </tr>
+    <tr>
+        <th>
+            <img src="image/Kaito_img.png" alt="Kaito">
+        </th>
+        <th>
+            <img src="image/Kibo_img.png" alt="Kibo">
+        </th>
+        <th>
+            <img src="image/Kirumi_img.png" alt="Kirumi">
+        </th>
+        <th>
+            <img src="image/Kokichi_img.png" alt="Kokichi">
+        </th>
+    </tr>
+    <tr>
+        <th>
+            <img src="image/Korekiyo_img.png" alt="Korekiyo">
+        </th>
+        <th>
+            <img src="image/Maki_img.png" alt="Maki">
+        </th>
+        <th>
+            <img src="image/Miu_img.png" alt="Miu">
+        </th>
+        <th>
+            <img src="image/Rantaro_img.png" alt="Rantaro">
+        </th>
+    </tr>
+    <tr>
+        <th>
+            <img src="image/Ryoma_img.png" alt="Ryoma">
+        </th>
+        <th>
+            <img src="image/Shuichi_img.png" alt="Shuichi">
+        </th>
+        <th>
+            <img src="image/Tenko_img.png" alt="Tenko">
+        </th>
+        <th>
+            <img src="image/Tsumugi_img.png" alt="Tsumugi">
+        </th>
+    </tr>
+
+</table>
+
+</body>
+</html>
