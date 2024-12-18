afd4f92f8b8cd0e60dfbe5a2037e6ca4dfdc1f78
ancilloy <yael.ancillon@gmail.com>
2024-12-18 14:24:29 +0100

    Ajout de messages d'erreur, et update du calculateur pour respecter les PC.

--- statcalc.js
+++ statcalc.js
@@ -34,8 +34,8 @@ function other_frame(stat, base, lvl, nature) { // calculates a frame for   iv +
     return frm;
 }
 
-function random_matching_ev(frm, iv) {
-    min = Math.max(0  , (frm.lower - iv)*4    );
+function random_matching_ev(frm, iv, minimumValue=false) {
+    min = Math.max(0  , (frm.lower - iv)*4    ); if (minimumValue) { return min; }
     max = Math.min(255, (frm.upper - iv)*4 + 3);
     return min + Math.floor((max - min)*Math.random());
 }
@@ -55,32 +55,24 @@ function range(min, max, step=1) {
 // carac : object { statNo, modulo } or null if not given
 function processing(statMatrix, bstMap, nature, carac, hpTxt) {
     // Initializing the list of potential IVs
-    //console.log("Initializing the list of potential IVs");
-    possibleIVs = [];
+    let possibleIVs = [];
     for (let i=0; i<6; i++) { possibleIVs.push(range(32)); }
-    //console.log(possibleIVs);
 
     // Filter values depending on characteristics
     if (carac != null) {
-        //console.log("Filter values depending on characteristics");
         possibleIVs[carac.statNo] = possibleIVs[carac.statNo].filter(iv => (iv % 5) == carac.modulo);
-        //console.log(possibleIVs);
     }
 
     // Filter values depending on HP type
-    //console.log("Filter values depending on HP type");
     for (let statNo = 0; statNo < 6; statNo++) {
         if (hpTxt.charAt(statNo) != "?") {
             modulo = parseInt(hpTxt.charAt(statNo));
             possibleIVs[statNo] = possibleIVs[statNo].filter(iv => (iv % 2) == modulo);
         }
     }
-    //console.log(possibleIVs);
 
     // Filter values depending on given stats
-    //console.log("Filter values depending on given stats");
     for (let summaryNo = 0; summaryNo < statMatrix.length; summaryNo++) {
-        //console.log(`HP stat ${statMatrix[summaryNo][0]} at level ${statMatrix[summaryNo][6]}`);
         hpFrm = hp_frame(statMatrix[summaryNo][0], bstMap.get(statMatrix[summaryNo][7])[0], /* level : */ statMatrix[summaryNo][6]);
 
         // Getting the minimum possible IV value for this stat (mostly usable in the first summary), and filtering accordingly
@@ -91,13 +83,9 @@ function processing(statMatrix, bstMap, nature, carac, hpTxt) {
         maxIV = hpFrm.upper;
         if (possibleIVs[0][possibleIVs[0].length - 1] > maxIV) { possibleIVs[0] = possibleIVs[0].filter(iv => iv <= maxIV); }
 
-        //console.log(possibleIVs);
-
 
         for (let statNo = 1; statNo < 6; statNo++) {
-            //console.log(`${allstats[statNo]} stat ${statMatrix[summaryNo][statNo]} at level ${statMatrix[summaryNo][6]}`);
             statFrm = other_frame(statMatrix[summaryNo][statNo], bstMap.get(statMatrix[summaryNo][7])[statNo], /* level : */ statMatrix[summaryNo][6], nature[statNo]);
-            //console.log(statFrm);
 
             // Getting the minimum possible IV value for this stat (mostly usable in the first summary), and filtering accordingly
             minIV = statFrm.lower - 63;
@@ -106,18 +94,15 @@ function processing(statMatrix, bstMap, nature, carac, hpTxt) {
             // Same with the maximum possible IV value (mostly usable in the last summaries)
             maxIV = statFrm.upper;
             if (possibleIVs[statNo][possibleIVs[statNo].length - 1] > maxIV) { possibleIVs[statNo] = possibleIVs[statNo].filter(iv => iv <= maxIV); }
-
-            //console.log(possibleIVs);
         }
     }
 
+    // Filtering depending on the highest stat given by the characteristic
     if (carac!=null) {
-        //console.log("Filter values depending on characteristics - round 2");
         maxVal = possibleIVs[carac.statNo][possibleIVs[carac.statNo].length - 1];
         for (let statNo = 0; statNo < 6; statNo++) {
             possibleIVs[statNo] = possibleIVs[statNo].filter(iv => iv <= maxVal);
         }
-        //console.log(possibleIVs);
     }
 
     return possibleIVs;
@@ -138,22 +123,109 @@ function draw_test(size=10) {
     return C;
 }
 
-function sum(l) {
+function sum_attr(l, attr) {
     let s = 0;
-    l.forEach(x => { s+=x; });
+    l.forEach(x => { s+=x[attr]; });
     return s;
 }
 
-function selectIVs(possibleIVs, stats, bsts, nature) {
-    results = [];
-    while (results==[] || sum(results)>510)
-    results = [];
-    for (let statNo=0; statNo<6; statNo++) {
-        let iv = biased_draw(possibleIVs[statNo]);
-        let frm = (statNo==0) ? hp_frame(stats[statNo], bsts[statNo], stats[6]) : other_frame(stats[statNo], bsts[statNo], stats[6], nature[statNo]);
-        results.push({ iv: iv, ev: random_matching_ev(frm, iv) });
+function selectIVs(possibleIVs_base, stats, bsts, nature, pcFilter) {
+    let results = [];
+    let iterationCount = 0;
+    // The selection is repeated if the total of EVs exceeds 510
+    while (results.length==0 || sum_attr(results, "ev")>510) {
+        results = [];
+
+
+        // Emergency handle, if a solution hasn't been found after 5 tries
+        if (iterationCount>=5) {
+            return selectIVs_emergency(possibleIVs_base, stats, bsts, nature, pcFilter);
+        }
+
+
+        // If there are no Hidden Power constraints, we just use the full possible IVs pool.
+        let possibleIVs = possibleIVs_base;
+        if (pcFilter!=null) {
+
+            // The pcFilter lists all possible values for the binary number that decides Hidden Power type, bits sorted in stat order
+
+            // Take a copy of the possible IVs pool
+            possibleIVs = possibleIVs_base;
+            // Shuffling the filter, to avoid getting the same value all the time
+            let shuffledFilter = pcFilter.sort((a,b) => 0.5 - Math.random());
+            // Flag checking if we found a fitting filter
+            let failure = true;
+
+            // For every filter, we check if it could fit our possible IVs pool
+            for (let filterLine of shuffledFilter) {
+                for (let statNo=0; statNo<6; statNo++) {
+                    // Filter the values to fit the filter
+                    possibleIVs[statNo] = possibleIVs[statNo].filter((x) => (x%2)==filterLine[statNo]);
+                }
+
+                // If all stat have possible values, then we can stop this step
+                if (!possibleIVs.some(l => l.length==0)) {
+                    failure = false;
+                    break;
+                }
+            }
+            if (failure) {
+                // If the loop is exited without finding an adequate filter, it means there is probably an error with the data.
+                return null;
+            }
+        }
+        for (let statNo=0; statNo<6; statNo++) {
+            let ivPool = possibleIVs[statNo];
+            let iv = biased_draw(ivPool);
+            let frm = (statNo==0) ? hp_frame(stats[statNo], bsts[statNo], stats[6]) : other_frame(stats[statNo], bsts[statNo], stats[6], nature[statNo]);
+            results.push({ iv: iv, ev: random_matching_ev(frm, iv) });
+        }
+        iterationCount++;
     }
     return results;
 }
 
-//export { hp_stat, hp_frame, other_stat, other_frame };
+// Alternate version of selectIVs, that picks the most optimal values, to see if they even work.
+function selectIVs_emergency(possibleIVs_base, stats, bsts, nature, pcFilter) {
+    let results = [];
+    if (pcFilter==null) {
+        // Case 1 : no hidden power value
+        // We just get the optimal values : maximum IV, minimum EV in each stat
+        for (let statNo=0; statNo<6; statNo++) {
+            let ivPool = possibleIVs_base[statNo];
+            let iv = ivPool[ivPool.length - 1];
+            let frm = (statNo==0) ? hp_frame(stats[statNo], bsts[statNo], stats[6]) : other_frame(stats[statNo], bsts[statNo], stats[6], nature[statNo]);
+            results.push({ iv: iv, ev: random_matching_ev(frm, iv, minimumValue=true) });
+        }
+        // If this optimal result still has too much EVs, then there is no possible solution.
+        return (sum_attr(results, "ev")>510) ? null : results;
+
+    } else {
+        for (let filterLine of pcFilter) {
+            // Case 2 : we have a hidden power value to respect
+            // Same principle, but we test with every filter, until we find one working
+            results = [];
+            possibleIVs = possibleIVs_base;
+
+            for (let statNo=0; statNo<6; statNo++) {
+                // Filter the values to fit the filter
+                possibleIVs[statNo] = possibleIVs[statNo].filter((x) => (x%2)==filterLine[statNo]);
+            }
+
+            // If all stat have possible values, then we can pick values
+            if (!possibleIVs.some(l => l.length==0)) {
+                // We just get the optimal values : maximum IV, minimum EV in each stat
+                for (let statNo=0; statNo<6; statNo++) {
+                    let ivPool = possibleIVs[statNo];
+                    let iv = ivPool[ivPool.length - 1];
+                    let frm = (statNo==0) ? hp_frame(stats[statNo], bsts[statNo], stats[6]) : other_frame(stats[statNo], bsts[statNo], stats[6], nature[statNo]);
+                    results.push({ iv: iv, ev: random_matching_ev(frm, iv, minimumValue=true) });
+                }
+                // If this optimal result still has too much EVs, then there is no possible solution (with this filter).
+                if (sum_attr(results, "ev")<=510) { return results; };
+            }
+        }
+        // If we get here, then no filter brought any result
+        return null;
+    }
+}
